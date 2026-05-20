a66843354df600772a1a5dbacc66f7cb3d69cf95
FourWindff <msplima7@gmail.com>
2026-05-20 13:28:19 +0800

    chore: 精简 lab/mindmapworkflow.ts 的未使用导出

--- electron/lab/mindmapworkflow.ts
+++ electron/lab/mindmapworkflow.ts
@@ -1,23 +1,9 @@
 export type {
   AnthropicLabConfig,
-  ChatModelLike,
-  DocumentMeta,
-  LeafExtractionResult,
-  LoggerLike,
-  MergeGroup,
-  MergeTreeResult,
   MindmapWorkflowResult,
   MindmapYamlNode,
-  NormalizedAnthropicLabConfig,
-  PdfChunk,
   PdfPage,
-  PendingLeafRange,
-  WorkflowDependencies,
-  WorkflowError,
-  WorkflowLogEntry,
-  WorkflowMetricSnapshot,
-  WorkflowRuntime,
 } from './mindmapworkflow/types.js'
 
 export { loadPdfPages, chunkPdfPages, serializeMindmapYaml } from './mindmapworkflow/io.js'
-export { buildMindmapWorkflow, runMindmapWorkflow, __test__ } from './mindmapworkflow/workflow.js'
+export { runMindmapWorkflow, __test__ } from './mindmapworkflow/workflow.js'
